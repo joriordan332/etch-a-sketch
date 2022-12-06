94447867059ae04764145d179d3106130f8aa04d
Jonathan O'Riordan <jooriordan@hotmail.com>
2022-12-06 16:38:24 +0000

    Fixed the amount of square divs
    Updated the for loop so that instead of 16 square divs there is now 256 like requested
    Updated the css properties
    Added an event listener with a hover effect that changes the colour of the div when the mouse passes over them and also added a timeout that resets the colour after a short delay

--- etchasketch.js
+++ etchasketch.js
@@ -1,6 +1,17 @@
-for (let i = 0; i < 16; i++) {
-  const container = document.querySelector('#container');
+const container = document.querySelector('#container');
+
+for (let i = 0; i < 256; i++) {
   const gridSquares = document.createElement("gridSquares");
   gridSquares.classList.add('gridSquares');
   container.appendChild(gridSquares);
-}
+
+  gridSquares.addEventListener('mouseover', function mouseover(event) {
+     event.target.style.background = 'salmon';
+     setTimeout(() => {
+      event.target.style.background = "";
+    }, 500);
+  }, false);
+    };
+  
+
+
