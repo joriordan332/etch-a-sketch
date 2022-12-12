b8a8960ee39986b1cebd0787ac775060ee5f9b9d
Jonathan O'Riordan <jooriordan@hotmail.com>
2022-12-12 16:00:24 +0000

    Added button and prompt Button added on html document and an event listener that prompts the user when clicked asking how many squares per side Do while loop added so that the number of squares is between 1-100 Removed setTimeout function that previously changed the grid squares back to their original colour after a short delay when the mouse hovered over them

--- etchasketch.js
+++ etchasketch.js
@@ -1,17 +1,23 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('button')
 
-for (let i = 0; i < 256; i++) {
+  
+function game () {
+  let grid;
+  button.addEventListener('click',() => {
+  do {
+    grid = prompt("How many squares do you want per side 1-100?")
+  } while (grid > 100);
+    for (let i = 0; i < grid *2; i++) 
+  if (grid > 0 && grid <= 100) {
   const gridSquares = document.createElement("gridSquares");
   gridSquares.classList.add('gridSquares');
   container.appendChild(gridSquares);
-
   gridSquares.addEventListener('mouseover', function mouseover(event) {
      event.target.style.background = 'salmon';
-     setTimeout(() => {
-      event.target.style.background = "";
-    }, 500);
-  }, false);
-    };
-  
+    });
+  }
+});
+}
 
-
+game()
