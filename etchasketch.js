f517c25c0a450e4efc589c80477de04a4e084907
Jonathan O'Riordan <jooriordan@hotmail.com>
2022-12-14 10:30:06 +0000

    Added reset button and resetBoard function
    Reset button added that when clicked runs the resetBoard function and removes all of the divs that are created from the click me to start button

--- etchasketch.js
+++ etchasketch.js
@@ -1,23 +1,33 @@
 const container = document.querySelector('#container');
-const button = document.querySelector('button')
+const button = document.querySelector('button');
+
 
   
-function game () {
-  let grid;
-  button.addEventListener('click',() => {
-  do {
-    grid = prompt("How many squares do you want per side 1-100?")
-  } while (grid > 100);
-    for (let i = 0; i < grid *2; i++) 
-  if (grid > 0 && grid <= 100) {
-  const gridSquares = document.createElement("gridSquares");
-  gridSquares.classList.add('gridSquares');
-  container.appendChild(gridSquares);
-  gridSquares.addEventListener('mouseover', function mouseover(event) {
-     event.target.style.background = 'salmon';
-    });
+
+
+  function game() {
+    let grid;
+    button.addEventListener('click',() => {
+    do {
+      grid = prompt("How many squares do you want per side 1-100?")
+    } while (grid > 100);
+      for (let i = 0; i < grid *2; i++) 
+    if (grid > 0 && grid <= 100) {
+    const gridSquares = document.createElement("gridSquares");
+    gridSquares.classList.add('gridSquares');
+    container.appendChild(gridSquares);
+    gridSquares.addEventListener('mouseover', function mouseover(event) {
+       event.target.style.background = 'salmon';
+      });
+    }
+  });
   }
-});
+  
+  function resetBoard() {
+    let board = document.querySelector("#container");
+    let squares = container.querySelectorAll("gridSquares");
+    squares.forEach((gridSquares) => (gridSquares.remove()));
+    squares.forEach((gridSquares) => (gridSquares.style.border = "white"));
 }
 
 game()
