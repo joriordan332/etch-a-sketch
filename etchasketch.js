4a82869f46b72c4eeebdc9242a04b45ff10f03a6
Jonathan O'Riordan <jooriordan@hotmail.com>
2022-12-21 13:42:52 +0000

    Added different funtionality to the html and js
    Changed button that prompted user when clicked in favour of an input box where the user can input a number 2-100
    Added a for loop that had a 16 x 16 already on the page
    Added parameter size in my game function that allows the existing grid to be removed and generated in the same total space as before
    style properties added gridTemplateColumns & gridTemplateRows to generate grid
    ChangeSize function added so input will be between 2-100 or an error message will appear

--- etchasketch.js
+++ etchasketch.js
@@ -1,33 +1,51 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.grid');
 const button = document.querySelector('button');
 
-
+for (let i = 0; i < 256; i++) {
+  const gridSquares = document.createElement('gridSquares');
+  gridSquares.classList.add('gridSquares');
+  container.appendChild(gridSquares);
+  
+  gridSquares.addEventListener('mouseover', function mouseover(event) {
+  event.target.style.background = 'black';
+  });
+}
   
 
 
-  function game() {
-    let grid;
-    button.addEventListener('click',() => {
-    do {
-      grid = prompt("How many squares do you want per side 1-100?")
-    } while (grid > 100);
-      for (let i = 0; i < grid *2; i++) 
-    if (grid > 0 && grid <= 100) {
+function game(size) {
+    
+    let squares = container.querySelectorAll('gridSquares');
+    squares.forEach((gridSquares) => (gridSquares.remove()));
+    
+    let grid = size * size
+    for (let i = 0; i < grid; i++){ 
+    let grid = document.querySelector(".grid")
     const gridSquares = document.createElement("gridSquares");
     gridSquares.classList.add('gridSquares');
     container.appendChild(gridSquares);
+    grid.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    
+
     gridSquares.addEventListener('mouseover', function mouseover(event) {
-       event.target.style.background = 'salmon';
-      });
+     event.target.style.background = 'black';
+    });
     }
-  });
+}
+game(16)
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    document.querySelector(".error").style.display = "none";
+    game(input);
+  } else {
+    document.querySelector(".error").style.display = "flex";
   }
-  
-  function resetBoard() {
-    let board = document.querySelector("#container");
-    let squares = container.querySelectorAll("gridSquares");
-    squares.forEach((gridSquares) => (gridSquares.remove()));
-    squares.forEach((gridSquares) => (gridSquares.style.border = "white"));
 }
 
-game()
+  function resetBoard() {
+    let squares = container.querySelectorAll('gridSquares');
+    squares.forEach((gridSquares) => (gridSquares.remove()));
+}
+
